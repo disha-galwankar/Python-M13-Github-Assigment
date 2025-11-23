7b3d857f7a6c7d74786e371ee0e38f5519834a03
Disha Galwankar <dishasgalwankar@gmail.com>
2025-11-23 18:02:59 -0500

    Clean up code, add comments, debug, and finalize program

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -1,15 +1,20 @@
+# Display assignment welcome message
 print("Welcome to my Python Github Assignment!")
 
-monthly_spending = input("How much did you spend this month?")
+# Create input for monthly spending and convert to float with error handling for non-numeric inputs
+while True:
+    monthly_spending = input("How much did you spend this month?")  
+    try:
+        monthly_spending = float(monthly_spending)
+        break
+    except ValueError:
+        print("Please enter a valid number for your monthly spending.")
 
-monthly_spending = float(monthly_spending)
+# Calculate yearly spending using monthly spending
 yearly_spending = monthly_spending * 12
 
+# Print yearly spending message and respond to user
 print(f"With your current monthly spending, you are on track to spend {yearly_spending} dollars this year!")
 
-try:
-    monthly_spending = float(monthly_spending)
-except ValueError:
-    print("Please enter a valid number for your spending.")
-    exit()
+
 
