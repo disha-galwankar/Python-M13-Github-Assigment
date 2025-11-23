6e8f1ea90a9150ffd2459c5ed24b0552d0291938
Disha Galwankar <dishasgalwankar@gmail.com>
2025-11-23 17:45:13 -0500

    Add formatted output displaying yearly_spending calculation results

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -4,3 +4,5 @@ spending = input("How much did you spend this month?")
 
 spending = float(spending)
 yearly_spending = spending * 12
+
+print(f"With your current monthly spending, you are on track to spend {yearly_spending} dollars this year!")
