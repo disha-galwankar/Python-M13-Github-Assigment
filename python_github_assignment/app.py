169a42183a47d03172bff04f9a1b51fb1391e9f4
Disha Galwankar <dishasgalwankar@gmail.com>
2025-11-23 17:51:56 -0500

    Change variable label to increase code clarity

--- python_github_assignment/app.py
+++ python_github_assignment/app.py
@@ -1,14 +1,14 @@
 print("Welcome to my Python Github Assignment!")
 
-spending = input("How much did you spend this month?")
+monthly_spending = input("How much did you spend this month?")
 
-spending = float(spending)
-yearly_spending = spending * 12
+monthly_spending = float(monthly_spending)
+yearly_spending = monthly_spending * 12
 
 print(f"With your current monthly spending, you are on track to spend {yearly_spending} dollars this year!")
 
 try:
-    spending = float(spending)
+    monthly_spending = float(monthly_spending)
 except ValueError:
     print("Please enter a valid number for your spending.")
     exit()
